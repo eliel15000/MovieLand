533d732d3f0b8a98dca15ec41772e0e3007cdbaa
eliel15000 <eliezercoding@gmail.com>
2022-12-08 18:57:59 -0500

    API_URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return(
     <div className="App">
       <h1>App</h1>
